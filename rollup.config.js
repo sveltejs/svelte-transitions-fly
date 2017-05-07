4a363d5193e9805f426dce32fdc4e352d67bbb19
Rich-Harris <richard.a.harris@gmail.com>
2017-05-07 10:25:54 -0400

    tweak config

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ const pkg = require('./package.json');
 export default {
 	entry: 'src/index.js',
 	plugins: [buble()],
+	external: ['eases-jsnext'],
 	targets: [
 		{ dest: pkg.main, format: 'cjs' },
 		{ dest: pkg.module, format: 'es' }
