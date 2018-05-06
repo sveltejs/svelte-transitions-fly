1634333b2fd2e5e3ecc795bf6ee4176402b8fff9
Rich Harris <richard.a.harris@gmail.com>
2018-05-06 17:39:59 -0400

    respect existing transforms

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import { cubicOut } from 'eases-jsnext';
 
-export default function fly(
-	node,
-	{ delay = 0, duration = 400, easing = cubicOut, x = 0, y = 0 }
-) {
-	const o = +getComputedStyle(node).opacity;
+export default function fly(node, {
+	delay = 0,
+	duration = 400,
+	easing = cubicOut,
+	x = 0,
+	y = 0
+}) {
+	const { opacity, transform } = getComputedStyle(node);
 
 	return {
 		delay,
 		duration,
 		easing,
-		css: t =>
-			`transform: translate(${(1 - t) * x}px, ${(1 - t) * y}px); opacity: ${t * o}`
+		css: t => `
+			transform: ${transform} translate(${(1 - t) * x}px, ${(1 - t) * y}px);
+			opacity: ${t * opacity}`
 	};
 }
